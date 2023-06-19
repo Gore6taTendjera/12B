b18b893d5851fc34fbc855c39a07463d930c8102
Gore6taTendjera <115811343+Gore6taTendjera@users.noreply.github.com>
2023-06-19 22:20:16 +0300

    ABITURIENTSKA

---
+++ abiturientska/thumb/kd.py
@@ -0,0 +1,30 @@
+from PIL import Image
+import os
+
+def reduce_resolution(image_path):
+    image = Image.open(image_path)
+    width, height = image.size
+    new_width = width // 2
+    new_height = height // 2
+    resized_image = image.resize((new_width, new_height))
+
+    # Create a new folder to store the resized images if it doesn't exist
+    output_folder = "resized"
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Construct the output file path
+    file_name = os.path.basename(image_path)
+    output_path = os.path.join(output_folder, file_name)
+
+    resized_image.save(output_path)
+
+def process_webp_files():
+    current_directory = os.getcwd()
+    for file_name in os.listdir(current_directory):
+        if file_name.endswith(".webp"):
+            file_path = os.path.join(current_directory, file_name)
+            reduce_resolution(file_path)
+            print(f"Reduced resolution of {file_name}")
+
+# Example usage:
+process_webp_files()
